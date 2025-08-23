184d7354387a9e4c99607905f6883a460d3bcc77
thesvieamor <thesvieamor@gmail.com>
2025-08-24 00:37:39 +0800

    Add files via upload

---
+++ display.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Student Info</title>
+</head>
+<body>
+    Student Id No: <?php echo $_POST["IdNum"]; ?> <br>
+    Your First Name: <?php echo $_POST["firstName"]; ?> <br>
+    Your Last Name: <?php echo $_POST["lastName"]; ?> <br>
+    Age: <?php echo $_POST["age"]; ?> <br>
+    Course: <?php echo $_POST["course"]; ?> <br>
+</body>
+</html>
